c589e9520833b0f06d2245de38759fb35bfb2450
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-03-08 18:44:23 +0100

    Added headers to Question class in Admin portal

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,13 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
